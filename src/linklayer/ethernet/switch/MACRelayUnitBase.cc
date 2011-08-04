a65c9944520fcb88c32748560c36764c42716f50
Zoltan Bojthe <zbojthe@urdeso.hu>
2011-08-04 15:01:53 +0200

    ethernet: rename PAUSE_BITTIME to PAUSE_UNIT_BITS and use it
    X-Committed-on-Branch: topic/etherfixes

--- src/linklayer/ethernet/switch/MACRelayUnitBase.cc
+++ src/linklayer/ethernet/switch/MACRelayUnitBase.cc
@@ -317,7 +317,7 @@ void MACRelayUnitBase::sendPauseFrame(int portno, int pauseUnits)
     if (destModule->isActive())
     {
         send(frame, gate);
-        pauseFinished[portno] = simTime() + 512.0 * pauseUnits / destModule->getTxRate();
+        pauseFinished[portno] = simTime() + ((double)PAUSE_UNIT_BITS) * pauseUnits / destModule->getTxRate();
     }
     else //disconnected or disabled
         pauseFinished[portno] = SIMTIME_ZERO;
