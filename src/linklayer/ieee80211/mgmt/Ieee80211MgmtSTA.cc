c4bddafd7fc13ec8ed00ed8c67f69127df3a7332
Zoltan Bojthe <zbojthe@urdeso.hu>
2011-09-14 17:28:36 +0200

    fixes for bug #190 by Alfonso Ariza: 802.11 STA nodes abort the simulation
    If the STA node is not associated to an AP it will send a packet with a invalid address to the mac and the mac will stop the simulation.
    It's necessary to modify the files Ieee80211STA.cc and Ieee80211STASimple.cc
    
    Copied changes from bugreport and from inetmanet.
    
    In Ieee80211MgmtSTA, I removed the old solution, and insert new solution from manet.

--- src/linklayer/ieee80211/mgmt/Ieee80211MgmtSTA.cc
+++ src/linklayer/ieee80211/mgmt/Ieee80211MgmtSTA.cc
@@ -178,13 +178,15 @@ void Ieee80211MgmtSTA::handleTimer(cMessage *msg)
 
 void Ieee80211MgmtSTA::handleUpperMessage(cPacket *msg)
 {
-    Ieee80211DataFrame *frame = encapsulate(msg);
+    if (!isAssociated || assocAP.address.isUnspecified())
+    {
+        EV << "The STA is not associated to an Access point, discard the packet" << msg << "\n";
+        delete msg;
+        return;
+    }
 
-    // Discard frame if STA is not associated (assocAP.address is unspecified).
-    if (frame->getReceiverAddress().isUnspecified())
-        delete frame;
-    else
-        sendOrEnqueue(frame);
+    Ieee80211DataFrame *frame = encapsulate(msg);
+    sendOrEnqueue(frame);
 }
 
 void Ieee80211MgmtSTA::handleCommand(int msgkind, cObject *ctrl)
